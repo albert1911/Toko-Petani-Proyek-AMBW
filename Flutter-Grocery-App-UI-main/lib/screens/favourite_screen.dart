0b3e9c7b23b52f53ac38e6039c5e25d36dc72939
toni ariyanto putra <c14200121@john.petra.ac.id>
2023-06-16 16:57:14 +0800

    favorite
    favourite screen

--- Flutter-Grocery-App-UI-main/lib/screens/favourite_screen.dart
+++ Flutter-Grocery-App-UI-main/lib/screens/favourite_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:grocery_app/common_widgets/app_text.dart';
+import 'package:grocery_app/screens/product_details/product_details_screen.dart';
+
+import '../models/grocery_item.dart';
+import '../widgets/grocery_item_card_widget.dart';
 
 
 class FavouriteScreen extends StatelessWidget {
@@ -15,4 +20,42 @@ class FavouriteScreen extends StatelessWidget {
       ),
     );
   }
+  Widget getHorizontalItemSlider(List<GroceryItem> items) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      height: 300,
+      child: ListView.separated(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        itemCount: items.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              onItemClicked(context, items[index]);
+            },
+            child: GroceryItemCardWidget(
+              item: items[index],
+              heroSuffix: "home_screen",
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return SizedBox(
+            width: 20,
+          );
+        },
+      ),
+    );
+  }
+  void onItemClicked(BuildContext context, GroceryItem groceryItem) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => ProductDetailsScreen(
+                groceryItem,
+                heroSuffix: "home_screen",
+              )),
+    );
+  }
+  
 }
